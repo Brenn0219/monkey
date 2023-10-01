471119f39be2da44478d21855f98d077c0d9a08a
brenn0219 <brennoaugusto2109@gmail.com>
2023-10-01 14:31:32 -0300

    analyzing identifiers

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -1,18 +1,20 @@
 package lexer
 
-import "monkey/token"
+import (
+	"./token"
+)
 
 type Lexer struct {
-	input string
-	position int // posicao atual na entrada (aponta para o caractere atual)
-	readPosition int // posicao dae leitura atual na entrada (apos o caractere atual)
-	ch byte // caracter atual de examinacao 
+	input        string
+	position     int  // posicao atual na entrada (aponta para o caractere atual)
+	readPosition int  // posicao dae leitura atual na entrada (apos o caractere atual)
+	ch           byte // caracter atual de examinacao
 }
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	l.readChar()
-	return l;
+	return l
 }
 
 func (l *Lexer) readChar() {
@@ -48,6 +50,13 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
@@ -56,4 +65,18 @@ func (l *Lexer) NextToken() token.Token {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
